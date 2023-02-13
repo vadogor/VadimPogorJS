f2cd35c77d6e360ab38f56567614b00a761642bc
vadogor <vp2010.vadogor@gmail.com>
2023-02-13 15:50:54 +0200

    task 1

---
+++ Learning/Homework/13.02.2023/web.js
@@ -0,0 +1,30 @@
+//Practical tasks for JavaScript fundamentals
+
+//Task 1
+let n = prompt("Introduceti o cifra", 9);
+switch(Number(n)) {
+    case 1: console.log("One"); 
+    break;
+    case 2: console.log("Two");
+    break;
+    case 3: console.log("Three");
+    break;
+    case 4: console.log("Four");
+    break;
+    case 5: console.log("Five");
+    break;
+    case 6: console.log("Six");
+    break;
+    case 7: console.log("Seven");
+    break;
+    case 8: console.log("Eight");
+    break;
+    case 9: console.log("Nine");
+    break;
+    case 0: console.log("Zero");
+    break;
+
+    default: console.log('Cred ca nu ati introdus o cifra')
+    break;
+}
+
