89f0d0cdb87577a421bccd0c9ea22bc54d7fd40e
vadogor <vp2010.vadogor@gmail.com>
2023-02-13 15:51:36 +0200

    task 2

--- Learning/Homework/13.02.2023/web.js
+++ Learning/Homework/13.02.2023/web.js
@@ -1,7 +1,7 @@
 //Practical tasks for JavaScript fundamentals
 
 //Task 1
-let n = prompt("Introduceti o cifra", 9);
+/*let n = prompt("Introduceti o cifra", 9);
 switch(Number(n)) {
     case 1: console.log("One"); 
     break;
@@ -26,5 +26,39 @@ switch(Number(n)) {
 
     default: console.log('Cred ca nu ati introdus o cifra')
     break;
+}*/
+
+//Task 2
+alert('In continuare urmeaza sa introduceti un interval [a, b], cu a, b - numere pozitive');
+let x = null;
+let y = null;
+do{
+    x = prompt("Introduceti inceputul intervalului", 0);
+} while (x < 0);
+
+do{
+    y = prompt("Introduceti sfarsitul intervalului", 100);
+} while(y < 0);
+
+let s = 0;
+let m = x / 1;
+let o = y / 1;
+if(m == o){ 
+    console.log(`[${x},${y}] nu este un interval`);
+} else {
+    if(m > o){
+        let v = m;
+        m = o;
+        o = v;
+    } 
+    if(m % 2 === 1){
+        m = m + 1;
+    }
+    let i = m;       
+    while(i <= o){
+        s += i;
+        i += 2;
+    }
+    console.log(`Suma numerelor pare din intervalul [${x},${y}] este: ${s}`);
 }
 
